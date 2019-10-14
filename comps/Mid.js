4cf4a9f8544da32b064bf4468e5e0c0d41b7c15b
siyayang0420 <siyayang0420@live.com>
2019-10-13 18:08:24 -0700

    Bot finished

--- comps/Mid.js
+++ comps/Mid.js
@@ -1,8 +1,11 @@
-import React from 'react-native';
+import React from 'react';
+import {View,Text} from 'react-native';
 
 function Mid(){
     return (
+        <View>
 
+        </View>
     );
 }
 
